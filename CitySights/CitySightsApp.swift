1e5296f514d42f59374702c573f12eee7474180a
Cory DeGarmo <degarmo@gmail.com>
2022-03-17 13:05:28 -0500

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by CD on 3/17/22.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
