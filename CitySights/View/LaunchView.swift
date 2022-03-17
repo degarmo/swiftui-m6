1545663a4d3e2a1847d775f6c354372e52c23c96
Cory DeGarmo <degarmo@gmail.com>
2022-03-17 13:18:03 -0500

    built initial folders and renamed views and ready to build.

--- CitySights/View/LaunchView.swift
+++ CitySights/View/LaunchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LaunchView.swift
 //  CitySights
 //
 //  Created by CD on 3/17/22.
@@ -7,15 +7,15 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LaunchView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct LaunchView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LaunchView()
     }
 }
